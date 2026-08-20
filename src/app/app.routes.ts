dd8503138b19c9ae772396e6434658e40735bc99
Arthur Ranegl Silveira Golfette Lopes <artrangel07@gmail.com>
2026-08-19 22:18:28 -0300

    Primeiro componente home-funcionario com solicitações em aberto, RF011

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeClienteComponent } from './components/home-cliente/home-cliente.component';
 import { LoginComponent } from './components/login/login.component';
 import { AutocadastroComponent } from './components/autocadastro/autocadastro.component';
+import { HomeFuncionario } from './components/home-funcionario/home-funcionario';
 
 export const routes: Routes = [
   { path: '', component: HomeClienteComponent },
   { path: 'login', component: LoginComponent },
   { path: 'autocadastro', component: AutocadastroComponent },
+  { path: 'home-funcionario', component: HomeFuncionario },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/components/home-funcionario/home-funcionario.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeFuncionario } from './home-funcionario';
+
+describe('HomeFuncionario', () => {
+  let component: HomeFuncionario;
+  let fixture: ComponentFixture<HomeFuncionario>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HomeFuncionario],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomeFuncionario);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
