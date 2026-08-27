d587bbf6d6efce00e22d3166090103d3d7726ca5
Rodrigo Yuji <yuji.rodrigo@ufpr.br>
2026-08-27 08:49:55 -0300

    Criação de tela de nova solicitação (Manutenção)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { AutocadastroComponent } from './components/autocadastro/autocadastro.component';
 import { HomeFuncionario } from './components/home-funcionario/home-funcionario';
 import { EfetuarOrcamento } from './components/efetuar-orcamento/efetuar-orcamento';
+import { NovaSolicitacao } from './components/nova-solicitacao/nova-solicitacao';
 
 export const routes: Routes = [
   { path: '', component: HomeClienteComponent },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'autocadastro', component: AutocadastroComponent },
   { path: 'home-funcionario', component: HomeFuncionario },
   { path: 'efetuar-orcamento', component: EfetuarOrcamento },
+  { path: 'nova-solicitacao', component: NovaSolicitacao },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/components/nova-solicitacao/nova-solicitacao.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-solicitacao',
+  imports: [],
+  templateUrl: './nova-solicitacao.html',
+  styleUrl: './nova-solicitacao.css',
+})
+export class NovaSolicitacao {}
